92d10d680069db951daeb54c3d344f78f38930d2
Ashley Joseph <ashley.joseph@tooeleschools.org>
2019-09-04 14:13:34 -0600

    ch3 projects

---
+++ example projects/src/ajoseph_example_projects/About_Me.java
@@ -0,0 +1,71 @@
+/**
+ * 
+ */
+package ajoseph_example_projects;
+
+/**
+ * @author ashley.joseph
+ * project: About Me
+ * date: 9/19
+ *
+ */
+public class About_Me 
+{
+
+	/**
+	 * 
+	 */
+	public About_Me() 
+	{
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) 
+	{
+		System.out.println("Ashley Joseph");
+			System.out.println("(Times based on typical bell scedule)");
+			System.out.println("");
+		
+		System.out.println("Period 1 Finacial Literacy");
+			System.out.println("	8:00 AM to 9:10 AM");
+			System.out.println("");
+		
+		System.out.println("Period 2 AP English Literature");
+			System.out.println("	9:15 AM to 10:25 AM");
+			System.out.println("");
+		
+		System.out.println("Period 3 AP Calculus");
+			System.out.println("	10:30 AM to 11:40 AM");
+			System.out.println("");
+		
+		System.out.println("Period 4 Release Time - Seminary");
+			System.out.println("	With 1st Lunch 12:15 to 1:25");
+			System.out.println("");
+		
+		System.out.println("Period 5 Release to Parent");
+			System.out.println("	C:");
+			System.out.println("");
+		
+		System.out.println("Period 6 TA - Ms. Kahlo");
+			System.out.println("	8:00 AM to 9:10 AM");
+			System.out.println("");
+		
+		System.out.println("Period 7 Chamber Orchestra");
+			System.out.println("	9:15 AM to 10:25 AM");
+			System.out.println("");
+		
+		System.out.println("Period 8 AP Calculus");
+			System.out.println("	C:");
+			System.out.println("");
+		
+		System.out.println("Period 9/10 Comp Programming 1 - Java");
+			System.out.println("	C:");
+			System.out.println("");
+		
+
+	}
+
+}
---
+++ example projects/src/ajoseph_example_projects/Hello_World.java
@@ -0,0 +1,33 @@
+/**
+ * 
+ */
+package ajoseph_example_projects;
+
+/**
+ * @author ashley.joseph
+ * project name: Hello World
+ * date: 9/19
+ *
+ */
+
+public class Hello_World {
+
+	/**
+	 * 
+	 */
+	public Hello_World() {
+		System.out.println("Hello World");
+		System.out.println("again");
+		String hello = "hello";
+		System.out.println(hello);
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		new Hello_World();
+
+	}
+
+}
