beb496c84222fb55b316b01b48b2b34f0f855ca7
Ashley Joseph <ashley.joseph021@gmail.com>
2019-09-09 19:41:45 -0600

    finished all ex projects

---
+++ example projects/src/ajoseph_example_projects/Goodbye.java
@@ -0,0 +1,24 @@
+package ajoseph_example_projects;
+
+/**
+ * @author ashley.joseph
+ * project: About Me
+ * date: 9/19
+ *
+ */
+
+public class Goodbye 
+{
+
+	public static void main(String[] args) 
+	{
+		
+		System.out.println("In Portuguese Adeus");
+		System.out.println("In Italian Arrivederci");
+		System.out.println("In Romanian La revedere");
+		System.out.println("In Dutch Tot ziens");
+		System.out.println("In Swedish Hejdå");
+
+	}
+
+}
---
+++ example projects/src/ajoseph_example_projects/Hello_World2.java
@@ -0,0 +1,30 @@
+package ajoseph_example_projects;
+
+/**
+ * @author ashley.joseph
+ * project name: Hello World
+ * date: 9/19
+ *
+ */
+
+public class Hello_World2 {
+
+	/**
+	 * 
+	 */
+	public Hello_World2() {
+		System.out.println("Hello World");
+		System.out.println("again");
+		String hello = "hello";
+		System.out.println(hello);
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		new Hello_World2();
+
+	}
+
+}
